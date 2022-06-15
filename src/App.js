f782bd8cd6aad3376a6a909d5e00cbc85b19c29a
vikhulei <vikhulei@gmail.com>
2022-06-15 12:16:34 +0100

    Input component updated

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 // import Navbar from "./components/Navbar"
 // import Index from "./components/Flipcard"
 import Grid from "./components/Grid"
+import InputForm from "./components/Input"
 
 function App() {
   return (
     <div>
       {/* <Index /> */}
       {/* <Navbar /> */}
-      <Grid />
+      {/* <Grid /> */}
+      <InputForm />
     </div>
   );
 }
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,5 +1,6 @@
 import styled from "styled-components"
 import tank from "../assets/tank.jpg"
+import me from "../assets/me.png"
 
 const Wrapper = styled.div `
 position: relative;
@@ -12,10 +13,26 @@ const Card = styled.div `
 position: absolute;
 top: 20%;
 left: 20%;
-width: 40%;
-height: 30%;
-background-color: blue;
+width: 15%;
+height: 60%;
+// background-color: blue;
 // text-align: center;
+`
+
+const ImageWrapper = styled.div `
+position: relative;
+width: 100%;
+background-color: gray;
+`
+
+const Image = styled.img `
+position: relative;
+top 0;
+margin-bottom: -5px;
+margin-top: -15%;
+// object-fit: contain;
+width: 100%;
+
 `
 
 const TitleWrapper = styled.div `
@@ -32,22 +49,47 @@ font-weight: bold;
 // background-color: red;
 `
 
-const Image = styled.img `
+const Wrapper1 = styled.div `
 position: absolute;
-object-fit: contain;
+top: 100px;
+left: 200px;
+height: 400px;
+width: 200px;
+background-color: lightblue;
+`
+
+const Box1 = styled.div `
+position: relative;
+// height: 50%;
 width: 100%;
+background-color: green;
+`
+
+const BoxInBox = styled.div `
+position: relative;
+height: 50%;
+width: 100%;
+background-color: lightcoral;
 `
 
 const Grid = () => {
 
     return (
         <div>
-            <Wrapper>
+            {/* <Wrapper>
                 <Card>
                     <TitleWrapper><Title>Example</Title></TitleWrapper>
-                    
-                    <Image src={tank} alt="tank"></Image>
+                    <ImageWrapper>
+                    <Image src={me} alt="tank"></Image>
+                    </ImageWrapper>
                 </Card>
+            </Wrapper> */}
+            <Wrapper>
+            <Wrapper1>
+                <Box1>
+                <Image src={me} alt="tank"></Image>
+                </Box1>
+            </Wrapper1>
             </Wrapper>
         </div>
     )
